aec59418aa7d5a19172d5c69aedf4da154b9566c
Turchin_Alexander <ymeleca@gmail.com>
2022-07-18 09:17:29 +0300

    homework

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+print()
+
 list_1 = [1, 2, 3]
 list_2 = [10, 20, 30]
-dictionary = dict(zip(list_1, list_2))
+
+dictionary = {}
+for i in range(len(list_1)):
+    dictionary[list_1[i]] = list_2[i]
 print(dictionary)
 
 print()
-dictionary = dict.fromkeys([])
 
-for x in list_2:
-    for i in list_1:
-        dictionary = dict.fromkeys(list_1, x)
+list_1 = [1, 2, 3]
+list_2 = [10, 20, 30]
+
+x = 0
+dictionary = {}
+for i in list_1:
+  dictionary[i] = dictionary.get(i, 0) + list_2[x]
+  x += 1
+print(dictionary)
+
+print()
+
+list_1 = [1, 2, 3]
+list_2 = [10, 20, 30]
+
+x = 0
+dictionary = {}
+for i in list_1:
+  dictionary[i] = list_2[x]
+  x += 1
 print(dictionary)
