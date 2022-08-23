dd2701017a7c9181c54f732fa790bcd757deb8df
AleksandrTurchin <turchin.alexander.n@gmail.com>
2022-08-23 19:11:49 +0300

    changed Lesson16_files from v90Academy

---
+++ hgch.py
@@ -0,0 +1,14 @@
+#def example(*args, **kwargs):
+    #print(type(args))
+    #print(type(kwargs))
+#example(*args, **kwargs)
+
+#def func_decorator(func):
+    #def wrapper():
+        #print('-- что-то делаем перед вызовом функции --')
+        #func()
+        #print('-- что-то делаем после вызовом функции --')
+
+
+
+def my_dec(func)
